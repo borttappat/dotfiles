152b9d60a20bb725d21f11ecac4115aa4d0b8230
Andreas K <borttappat@proton.me>
2023-04-30 10:55:17 +0200

    fish config

--- config.fish
+++ config.fish
@@ -1,1 +1,14 @@
-/home/traum/.config/fish/config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+cat /home/traum/.cache/wal/sequences
+end
+
+# aliases
+alias reboot='systemctl reboot'
+alias ls='ls -A'
+
+
+
+set -U fish_greeting ""
+# end
