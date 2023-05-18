6016e14bcd4bd766b9c9d84b03e757e3f918d61b
AK <borttappat@proton.me>
2023-05-18 20:58:46 +0200

    updated file linking script

--- link_files.sh
+++ link_files.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
     # List of files to link
-    files=("config" "configuration.nix" "picom.conf" ".xinitc" "alacritty.yml" "Cerise.jpg" "config.fish" "config.rasi" "cozette.otb" "rc.conf" "rifle.conf" "Schematic.png" "startup.sh")
+    files=("config" "configuration.nix" "picom.conf" ".xinitc" "alacritty.yml" "Nervnasa.png" "config.fish" "config.rasi" "cozette.otb" "rc.conf" "rifle.conf" "Schematic.png" "startup.sh")
 
     # List of directories to link to
     directories=("~/.config/i3" "/etc/nixos" "~/.config/picom" "~/.config" "~/config/alacritty" "~/Wallpapers" "~/.config/fish" "~/.config/rofi" "~/.local/share/fonts" "~/.config/ranger" "~/.config/ranger" "~/Wallpapers" "~/.config/i3/scripts")
