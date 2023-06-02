d7fb0aeb7e48d4a149c162f6091a2680af6a1201
AK <borttappat@proton.me>
2023-06-03 01:12:54 +0200

    updates

--- link_files.sh
+++ link_files.sh
@@ -1,10 +1,10 @@
  #!/bin/bash
 
     # List of files to link
-    files=("config" "configuration.nix" "picom.conf" ".xinitc" "alacritty.yml" "Nervnasa.png" "config.fish" "config.rasi" "cozette.otb" "rc.conf" "rifle.conf" "Schematic.png" "startup.sh")
+    files=("config" "configuration.nix" "picom.conf" ".xinitc" "alacritty.yml" "Nervnasa.png" "config.fish" "config.rasi" "cozette.otb" "rc.conf" "rifle.conf" ".vimrc" "startup.sh")
 
     # List of directories to link to
-    directories=("~/.config/i3" "/etc/nixos" "~/.config/picom" "~/.config" "~/config/alacritty" "~/Wallpapers" "~/.config/fish" "~/.config/rofi" "~/.local/share/fonts" "~/.config/ranger" "~/.config/ranger" "~/Wallpapers" "~/.config/i3/scripts")
+    directories=("~/.config/i3" "/etc/nixos" "~/.config/picom" "~/.config" "~/config/alacritty" "~/Wallpapers" "~/.config/fish" "~/.config/rofi" "~/.local/share/fonts" "~/.config/ranger" "~/.config/ranger" "~/" "~/.config/i3/scripts")
 
     # Loop through the files and directories and call the Python
     script for each combination
