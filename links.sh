02452881322e5905aba7453bb07e69e91f2f4a62
AK <borttappat@proton.me>
2023-06-26 00:00:09 +0200

    updated links.sh

--- links.sh
+++ links.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo python link_file.py --files alacritty.yml,config.ini,rifle.conf,.bashrc,config.rasi,config,configuration.nix,picom.conf,.vimrc,config.fish,cozette.otb,rc.conf,.xinitrc --dirs ~/.config/alacritty,~/.config/polybar,~/.config/ranger,~/,~/.config/rofi,~/.config/polybar,/etc/nixos,~/.dotfiles/picom,~/,~/.config/fish,~/.local/share/fonts,~/.config/ranger,~/
+sudo python link_file.py --files alacritty.yml,config.ini,rifle.conf,.bashrc,config.rasi,config,configuration.nix,picom.conf,.vimrc,config.fish,cozette.otb,rc.conf,.xinitrc --dirs /home/traum/.config/alacritty,/home/traum/.config/polybar,/home/traum/.config/ranger,/home/traum,/home/traum/.config/rofi,/home/traum/.config/polybar,/etc/nixos,/home/traum/.config/picom,/home/traum/,/home/traum/.config/fish,/home/traum/.local/share/fonts,/home/traum/.config/ranger,/home/traum
