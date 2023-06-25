c7ea374f0367822de0689665c44d4914dd2b7342
AK <borttappat@proton.me>
2023-06-25 23:17:57 +0200

    script for linking file

---
+++ links.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo python link_file.py --files alacritty.yml,config.ini,rifle.conf,.bashrc,config.rasi,config,configuration.nix,picom.conf,.vimrc,config.fish,cozette.otb,rc.conf,.xinitrc --dirs ~/.config/alacritty,~/.config/polybar,~/.config/ranger,~/,~/.config/rofi,~/.config/polybar,/etc/nixos,~/.dotfiles/picom,~/,~/.config/fish,~/.local/share/fonts,~/.config/ranger,~/
