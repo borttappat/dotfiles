c6828526c4cd48b5783675602f98666281f6ad8b
AK <borttappat@proton.me>
2023-07-01 23:59:18 +0200

    updated walrgb to work with file-path and renamed the old script to randonwalrgb.sh for usage in i3

---
+++ randomwalrgb.sh
@@ -0,0 +1,6 @@
+#!/run/current-system/sw/bin/bash
+
+wal -i ~/Wallpapers -o wal-set
+
+HEX_CODE=$(sed -n '2p' ~/.cache/wal/colors | sed 's/#//')
+openrgb --device 0 --mode static --color ${HEX_CODE/#/}
--- walrgb.sh
+++ walrgb.sh
@@ -1,6 +1,23 @@
 #!/run/current-system/sw/bin/bash
 
-wal -i ~/Wallpapers -o wal-set
+# Check if the file path argument is provided
+    if [ -z "$1" ]; then
+      echo "Usage: ./script.sh /path/to/file"
+      exit 1
+    fi
+
+    # Extract specific part of the path using parameter expansion
+    file_path="$1"
+    file_name="${file_path##*/}"
+    directory="${file_path%/*}"
+
+    # Print the extracted parts of the path
+    echo "File path: $file_path"
+    echo "File name: $file_name"
+    echo "Directory: $directory"
+
+
+wal -i ${file_path}
 
 HEX_CODE=$(sed -n '2p' ~/.cache/wal/colors | sed 's/#//')
 openrgb --device 0 --mode static --color ${HEX_CODE/#/}
