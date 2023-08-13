7c782a5d5cec32dbd6ab8d6e1d95fc885fefe4c5
AK <borttappat@proton.me>
2023-08-13 16:04:49 +0200

    Splitting configuration.nix into modules

--- packages.nix
+++ packages.nix
@@ -4,17 +4,149 @@
 
  environment.systemPackages = with pkgs; [
 
+# Editors
+    vim
+    
+# Compilers
+    #gcc
+    python3
 
-    # recent ucategorized installs
+# Programs
+    librewolf
+    brave
+    mullvad-browser
+    tor
 
-    # Categorized
+# Terminal stuff
+    alacritty
+    htop
+    glances
+    neofetch
+    bunnyfetch
+    pfetch
+    nitch
+    cbonsai
+    cmatrix
+    ranger
+    figlet
+    ticker
+    tickrs
+    exa
     ttyper
-    terminal-typeracer
-    obsidian
-    opencl-info
-    sleuthkit
-    bully
-    openconnect
 
-    ];
+# WM
+    polybar
+    rofi
+    picom
+    #wpgtk
+    pywal
+    feh
+    #eww
+    #conky
+    betterlockscreen
+    i3lock-color
+    i3lock
+    #i3lock-fancy
+
+# Tools
+    openconnect
+    brightnessctl
+    obsidian
+    light 
+    #pciutils
+    undervolt
+    git
+    gh
+    chatgpt-cli
+    nmon
+    zathura
+    xdotool
+    killall
+    qemu
+    #kvmtool
+    tmux    
+    #onionshare
+    #picosnitch
+    flameshot
+    rar
+    pciutils
+    curl
+    wget
+    lshw
+
+    whois
+
+    uncover
+
+    wifite2
+    nikto
+    iw
+    nmap
+    wireshark
+    sherlock
+    iw
+    wirelesstools
+    hydra-cli
+    thc-hydra
+    metasploit
+    hashcat
+    cowpatty
+    hcxtools
+    aircrack-ng
+    
+    hostapd-mana
+    bully
+    sleuthkit
+    dbmonster
+    linux-router
+    pixiewps
+    theharvester
+    dirb
+    medusa
+    libargon2
+    kismet
+    airgeddon
+    steghide
+    stegseek
+    parsero
+    commix
+    cewl
+    bettercap
+    whatweb
+    reaverwps
+    reaverwps-t6x
+    john
+    netdiscover
+    lynis
+    fcrackzip
+    dnsrecon
+    socat
+    macchanger 
+    httrack
+    foremost
+    dnsenum
+    fierce
+    cryptsetup
+    wfuzz
+    testdisk
+    sqlmap
+    wpscan
+    tcpdump
+    ettercap
+    masscan
+    fping
+    evil-winrm
+    driftnet    
+    
+# X11
+    xorg.xinit
+    xorg.xrdb
+    xorg.xorgserver
+    
+# Uncategorized
+    unclutter
+    unzip
+    tealdeer
+    gimp
+];
 }
