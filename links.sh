d1a5facc3080156409a5ab40f4bae3836572de09
AK <borttappat@proton.me>
2023-09-23 23:11:11 +0200

    updates

--- links.sh
+++ links.sh
@@ -16,6 +16,7 @@ sudo python link_file.py --file users.nix --dirs /etc/nixos
 sudo python link_file.py --file services.nix --dirs /etc/nixos
 sudo python link_file.py --file nixp.nix --dirs /etc/nixos
 
+sudo python link_file.py --file MemoryFixed.png --dirs $HOME/Wallpapers
 echo "files linked"
 
 
