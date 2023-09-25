f8a3ba7bde7a2022c6a9f5ca39566b1646b2fd29
AK <borttappat@proton.me>
2023-09-25 23:39:13 +0200

    python script for setting up user-agnostic configuration.nix with i3

---
+++ i3setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import os
+
+# Define the lines to add
+line1 = "services.xserver.displayManager.startx.enable = true;"
+line2 = "services.xserver.windowManager.i3.enable = true;"
+
+# Set user_home variable to make script user-agnostic
+user_home = os.path.expanduser("~")
+
+#file_path = os.path.join(user_home, "etc/nixos/configuration.nix")
+#file_path = "/etc/nixos/configuration.nix"
+#file_path = "$HOME/test/testfile.txt"
+file_path = os.path.join(user_home, "test/testfile.txt")
+
+# Open the input file for reading
+with open(file_path, "r") as file:
+    lines = file.readlines()
+
+# Open the same file for writing, which will clear its contents
+with open(file_path, "w") as file:
+    replace_next_lines = False
+
+    for line in lines:
+        # Check if the line starts with "services.xserver"
+        if line.startswith("services.xserver"):
+            replace_next_lines = True
+        elif replace_next_lines:
+            # Replace the line with the new lines
+            file.write(line1 + "\n")
+            file.write(line2 + "\n")
+            replace_next_lines = False
+        else:
+            # If not, write the line back to the file
+            file.write(line)
+
+print("Lines starting with 'services.xserver' replaced with new lines successfully in /etc/nixos/configuration.nix.")
