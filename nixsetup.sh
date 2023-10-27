82271c18fa1fa7dfb257303f9dddc67023b57e98
AK <borttappat@proton.me>
2023-10-28 01:19:07 +0200

    updates

--- nixsetup.sh
+++ nixsetup.sh
@@ -21,26 +21,7 @@ sudo python link_file.py --files MemoryFixed.png --dirs $HOME/Wallpapers
 
 
 python userswitch.py
-
-# Adds a line allowing flakes to configuration.nix 
-new_line='nix.settings.experimental-features = [ "nix-command" "flakes" ];'
-
-# Define the file and line number
-file_path="/etc/nixos/configuration.nix"
-line_number=6
-
-# Check if the file exists
-if [ -f "$file_path" ]; then
-    # Add the new line to the file
-    sed -i "${line_number}i$new_line" "$file_path"
-    echo "Line added successfully."
-else
-    echo "File not found: $file_path"
-    exit 1
-fi
-
-# Removes existing xserver-settings and replaces the lines with i3-related lines
-#sudo python i3setup.py
+python nixboot.py
 
 # Rebuilds using flake, edit the name after # to your username after editing flake.nix in the same way
 sudo nixos-rebuild boot --flake /etc/nixos#traum
