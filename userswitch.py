a43038652ec9076152c2067e672a08cbe6f32c78
AK <borttappat@proton.me>
2023-10-28 00:11:07 +0200

    updates

--- userswitch.py
+++ userswitch.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 import os
-import fileinput
 
 # Function to replace text in a file
 def replace_text_in_file(file_path, search_text, replace_text):
-    with fileinput.FileInput(file_path, inplace=True) as file:
-        for line in file:
-            print(line.replace(search_text, replace_text), end='')
+    with open(file_path, 'r') as file:
+        file_content = file.read()
+    
+    modified_content = file_content.replace(search_text, replace_text)
+    
+    with open(file_path, 'w') as file:
+        file.write(modified_content)
 
-# User input for replacement text
-replace_text = input("Enter the text to replace 'traum' with: ")
+# Get the current user's name
+current_user = os.getlogin()
 
 # Directory containing the files to process
 directory = "~/dotfiles"  # Use the tilde character to represent the home directory
@@ -20,7 +23,6 @@ directory = os.path.expanduser(directory)
 # List of files to process (add more as needed)
 file_list = [
     "configuration.nix",
-    "services.nix",
     "users.nix",
     # Add more file names as needed
 ]
@@ -30,10 +32,10 @@ for file_name in file_list:
 
     # Check if the file exists before attempting to replace text
     if os.path.exists(file_path):
-        replace_text_in_file(file_path, "traum", replace_text)
-        print(f"Replaced 'traum' with '{replace_text}' in {file_name}")
+        replace_text_in_file(file_path, "traum", current_user)
+        print(f"Replaced 'traum' with '{current_user}' in {file_name}")
     else:
         print(f"File not found: {file_name}")
 
-print("Text replacement completed.")
+print("Username updated.")
 
