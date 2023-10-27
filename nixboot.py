962aa355961f728a3ddc5546777b3032db3c7b08
AK <borttappat@proton.me>
2023-10-28 00:57:42 +0200

    python script for preserving bootloader-settings from initial install

---
+++ nixboot.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import os
+
+# Define the file paths
+old_config_path = '/etc/nixos/configuration.nix.old'
+new_config_path = '/etc/nixos/configuration.nix'
+
+# Function to extract lines starting with "boot" from a file
+def extract_boot_lines(file_path):
+    boot_lines = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.strip().startswith("boot"):
+                boot_lines.append(line)
+    return boot_lines
+
+# Function to write lines to a file starting from a specific line
+def write_lines_to_file(file_path, lines, start_line):
+    with open(file_path, 'r') as file:
+        content = file.readlines()
+
+    # Insert the lines at the specified position
+    content[start_line:start_line] = lines
+
+    with open(file_path, 'w') as file:
+        file.writelines(content)
+
+# Extract boot lines from the old configuration
+boot_lines = extract_boot_lines(old_config_path)
+
+# Read the new configuration to determine where to insert the boot lines
+with open(new_config_path, 'r') as file:
+    new_config_content = file.readlines()
+
+# Find the line position of the last "boot" line in the new configuration
+last_boot_line_position = -1
+for i, line in enumerate(new_config_content):
+    if line.strip().startswith("boot"):
+        last_boot_line_position = i
+
+if last_boot_line_position >= 0:
+    # Append boot lines after the last "boot" line in the new configuration
+    write_lines_to_file(new_config_path, boot_lines, start_line=last_boot_line_position + 1)
+    print("Lines moved to the new configuration.")
+else:
+    print("No lines starting with 'boot' found in the old configuration or new configuration.")
+
