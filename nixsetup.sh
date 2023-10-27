a85fd693f4333d5789dcce1e64dcf0cf43913d88
AK <borttappat@proton.me>
2023-10-28 00:15:37 +0200

    updates

--- nixsetup.sh
+++ nixsetup.sh
@@ -20,26 +20,27 @@ sudo python link_file.py --files fish_variables  --dirs $HOME/.config/fish
 sudo python link_file.py --files MemoryFixed.png --dirs $HOME/Wallpapers
 
 
+python switchuser.py
 
 # Adds a line allowing flakes to configuration.nix 
-new_line='nix.settings.experimental-features = [ "nix-command" "flakes" ];'
+#new_line='nix.settings.experimental-features = [ "nix-command" "flakes" ];'
 
 # Define the file and line number
-file_path="/etc/nixos/configuration.nix"
-line_number=6
+#file_path="/etc/nixos/configuration.nix"
+#line_number=6
 
 # Check if the file exists
-if [ -f "$file_path" ]; then
+#if [ -f "$file_path" ]; then
     # Add the new line to the file
-    sed -i "${line_number}i$new_line" "$file_path"
-    echo "Line added successfully."
-else
-    echo "File not found: $file_path"
-    exit 1
+#    sed -i "${line_number}i$new_line" "$file_path"
+#    echo "Line added successfully."
+#else
+#    echo "File not found: $file_path"
+#    exit 1
 fi
 
 # Removes existing xserver-settings and replaces the lines with i3-related lines
-sudo python i3setup.py
+#sudo python i3setup.py
 
 # Rebuilds using flake, edit the name after # to your username after editing flake.nix in the same way
 sudo nixos-rebuild boot --flake /etc/nixos#traum
