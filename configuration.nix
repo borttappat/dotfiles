7cb90aa8abc480fe2c70a78863cea2fea2033d66
AK <borttappat@proton.me>
2023-11-02 12:12:55 +0100

    updates

--- configuration.nix
+++ configuration.nix
@@ -23,22 +23,6 @@
     ];
 
 
-# Modify the following like you would /etc/hosts
-    networking.extraHosts =
-    ''
-    10.129.240.39 unika.htb
-    10.129.72.251 thetoppers.htb
-    10.129.72.251 s3.thetoppers.htb
-    10.129.20.110 ignition.htb
-    10.10.11.233 analytical.htb
-    10.10.11.233 data.analytical.htb
-    10.10.11.227 tickets.keeper.htb/rt/
-    10.10.11.219 pilgrimage.htb
-    10.10.11.221 2million.htb
-    10.10.11.189 precious.htb
-    
-    '';
-
 # Bootloader
     # SystemD-boot
     boot.loader.systemd-boot.enable = true;
