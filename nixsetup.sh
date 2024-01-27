2d983e44f9e6f16bd157a863e24d13d7a2ac24b3
AK <borttappat@proton.me>
2024-01-28 00:50:01 +0100

    updated setup-script

--- nixsetup.sh
+++ nixsetup.sh
@@ -2,27 +2,45 @@
 
 sudo cp /etc/nixos/configuration.nix /etc/nixos/configuration.nix.old
 
-sudo python link_file.py --files alacritty.yml,config.ini,rifle.conf,.bashrc,config.rasi,config,picom.conf,.vimrc,config.fish,cozette.otb,rc.conf,.xinitrc --dirs $HOME/.config/alacritty,$HOME/.config/polybar,$HOME/.config/ranger,$HOME,$HOME/.config/rofi,$HOME/.config/i3,$HOME/.config/picom,$HOME/,$HOME/.config/fish,$HOME/.local/share/fonts,$HOME/.config/ranger,$HOME
+# misc
+sudo python link.py --file .ticker.yaml --dir ~/
+sudo python link.py --file .bashrc --dir ~/
+sudo python link.py --file .vimrc --dir ~/
+sudo python link.py --file .xinitrc --dir ~/
+sudo python link.py --file .Xmodmap --dir ~/
 
+sudo python link.py --file alacritty.toml --dir ~/.config/alacritty
+sudo python link.py --file config.rasi --dir ~/.config/rofi
+sudo python link.py --file config --dir ~/.config/i3
+sudo python link.py --file config.ini --dir ~/.config/polybar
+sudo python link.py --file joshuto.toml --dir ~/.config/joshuto
+sudo python link.py --file picom.conf --dir ~/.config/picom
 
-sudo python link_file.py --files .ticker.yaml --dirs ~/
+# fish
+sudo python link.py --file config.fish --dir ~/.config/fish
+sudo python link.py --file fish_variables  --dir ~/.config/fish
 
-# links for /etc/nixos
-sudo python link_file.py --files configuration.nix --dirs /etc/nixos
-sudo python link_file.py --files flake.nix --dirs /etc/nixos
-sudo python link_file.py --files packages.nix --dirs /etc/nixos
-sudo python link_file.py --files users.nix --dirs /etc/nixos
-sudo python link_file.py --files services.nix --dirs /etc/nixos
-sudo python link_file.py --files nixp.nix --dirs /etc/nixos
-sudo python link_file.py --files hosts.nix --dirs /etc/nixos
+# ranger
+sudo python link.py --file rifle.conf --dir ~/.config/ranger
+sudo python link.py --file rc.conf --dir ~/.config/ranger
 
-sudo python link_file.py --files fish_variables  --dirs $HOME/.config/fish
-sudo python link_file.py --files joshuto.toml --dirs $HOME/.config/joshuto
-sudo python link_file.py --files MemoryFixed.png --dirs $HOME/Wallpapers
+# nix
+sudo python link.py --file configuration.nix --dir /etc/nixos
+sudo python link.py --file flake.nix --dir /etc/nixos
+sudo python link.py --file packages.nix --dir /etc/nixos
+sudo python link.py --file users.nix --dir /etc/nixos
+sudo python link.py --file services.nix --dir /etc/nixos
+sudo python link.py --file nixp.nix --dir /etc/nixos
+sudo python link.py --file hosts.nix --dir /etc/nixos
+sudo python link.py --file razer.nix --dir /etc/nixos
+sudo python link.py --file asus.nix --dir /etc/nixos
+sudo python link.py --file steam.nix --dir /etc/nixos
 
+# make every script in ~/dotfiles executeable
+sudo chmod +x *.sh
 
 python userswitch.py
 python nixboot.py
 
 # Rebuilds using flake, edit the name after # to your username after editing flake.nix in the same way
-sudo nixos-rebuild boot --flake /etc/nixos#traum
+sudo nixbuild.sh
