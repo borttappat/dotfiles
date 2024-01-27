1ca2f871bf6fe7a73b1bc57600fc64999f5f09ed
AK <borttappat@proton.me>
2024-01-27 22:54:44 +0100

    specific setup for asus laptop

---
+++ asus.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+
+
+# Services
+
+services.asusd.enable = true;
+
+
+# Sound-extras
+    
+services.pipewire.jack.enable = true;
+services.jack.jackd.enable = false;
+
+
+# Packages
+
+environment.systemPackages = with pkgs; [
+
+asusctl
+
+];
+
+}
