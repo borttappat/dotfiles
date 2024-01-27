0900048a7d4d2ed77a1d52e454062dd9edb7cc64
AK <borttappat@proton.me>
2024-01-27 22:58:49 +0100

    reworked flake.nix to support different setups; razer, asus and WM

--- flake.nix
+++ flake.nix
@@ -1,59 +1,62 @@
-#     ______      __                _
-#    / __/ /___ _/ /_____    ____  (_)  __
-#   / /_/ / __ `/ //_/ _ \  / __ \/ / |/_/
-#  / __/ / /_/ / ,< /  __/ / / / / />  <
-# /_/ /_/\__,_/_/|_|\___(_)_/ /_/_/_/|_|
-
 {
-    description = "griefhounds NixOS flake";
-    
-    inputs = {
-    # There are many ways to reference flake inputs.
-    # The most widely used is `github:owner/name/reference`,
-    # which represents the GitHub repository URL + branch/commit-id/tag.
+    description = "Griefhounds NixOS Flake for multiple devices";
 
-    # Official NixOS package source, using nixos-unstable branch here
-    
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    #unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    #nixpkgs.url = github:NixOS/Nixpkgs;
-    #nixpkgs-unstable.url = github:MixOS/nixpkgs/nixos-unstable;
-    
-    #nixos-hardware.url = "github:nixos/nixos-hardware";
-     #nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    };
-    
-    outputs = { self, nixpkgs }:
-        let
-            user = "traum";
-            system = "x86_64-linux";
-            
-            pkgs = import nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-                };
-            
-            lib = nixpkgs.lib;
-        in {
+        inputs = {
 
-            nixosConfigurations = { 
-                ${user} = nixpkgs.lib.nixosSystem {
-                    inherit system;
-                    specialArgs = {inherit user;};
-                    modules = [ 
+            nixpkgs.url = "nixpkgs/nixos-unstable";
+            
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+};
+
+    outputs = { self, nixpkgs, ... }@inputs: {
+        nixosConfigurations = {
+            
+            "razer" = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                    modules = [
+                        ./hardware-configuration.nix
                         ./configuration.nix
-                        ./hardware-configuration.nix 
                         ./packages.nix
                         ./services.nix
+                        ./nixp.nix
                         ./users.nix
+                        ./hosts.nix
+                        ./razer.nix
+                    ];
+            };
+
+
+           "WM" = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                    modules = [
+                        ./hardware-configuration.nix
+                        ./configuration.nix
+                        ./packages.nix
+                        ./services.nix
+                        ./nixp.nix
+                        ./users.nix
+                        ./hosts.nix
+                    ];
+            } 
+
+
+            "asus" = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                    modules = [
+                        ./hardware-configuration.nix
+                        ./configuration.nix
+                        ./packages.nix
+                        ./services.nix
                         ./nixp.nix
                         ./hosts.nix
+                        ./users.nix
+                        ./asus.nix
+                        ./steam.nix
+                    ];
+            };
 
-                    ]; 
-                    };
-                };
         };
-}
 
+    };
+}
