81f65d544da37a587eaf20b910ba9401a2a71f25
AK <borttappat@proton.me>
2024-01-29 23:14:07 +0100

    moved bootloader from configuration.nix to boot.nix and modified nixboot.py to add relevant lines to boot.nix as a part of nixsetup.sh

--- configuration.nix
+++ configuration.nix
@@ -25,9 +25,9 @@
 
 # Bootloader
     # SystemD-boot
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.efi.efiSysMountPoint = "/boot/efi";
+    #boot.loader.systemd-boot.enable = true;
+    #boot.loader.efi.canTouchEfiVariables = true;
+    #boot.loader.efi.efiSysMountPoint = "/boot/efi";
     
 # Kernel
      boot.kernelPackages = pkgs.linuxPackages_latest;
