eee27443fe2a17f64e72df412ca55b5805ea0deb
AK <borttappat@proton.me>
2024-05-23 10:46:18 +0200

    updates

--- scripts/bash/randomwalrgb.sh
+++ scripts/bash/randomwalrgb.sh
@@ -2,7 +2,7 @@
 
 wal -q -i ~/Wallpapers -o wal-set
 
-HEX_CODE=$(sed -n '2p' ~/.cache/wal/colors | sed 's/#//')
+HEX_CODE=$(sed -n '5p' ~/.cache/wal/colors | sed 's/#//')
 openrgb --device 0 --mode static --color ${HEX_CODE/#/}
 
 polybar-msg cmd restart
