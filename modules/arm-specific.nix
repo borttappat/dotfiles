4f41c7fe39c524f8a37cd783899ab1169379584e
AK <borttappat@proton.me>
2025-04-01 13:00:53 +0200

    updates to keyboard functionality

--- modules/arm-specific.nix
+++ modules/arm-specific.nix
@@ -48,4 +48,61 @@
   # ARM-specific settings for audio
   sound.enable = true;
   hardware.pulseaudio.enable = true;
-
+  
+  # --- KEYBOARD INPUT FIXES ---
+  
+  # Ensure input drivers are available
+  services.xserver.libinput.enable = true;
+  
+  # Make sure evdev driver is included
+  services.xserver.modules = [ pkgs.xorg.xf86inputevdev ];
+  
+  # Add explicit keyboard configuration
+  services.xserver.xkbModel = "pc105";
+  
+  # Ensure all input devices have proper permissions
+  services.udev.extraRules = ''
+    # Input devices - ensure proper permissions
+    KERNEL=="event*", SUBSYSTEM=="input", MODE="0660", GROUP="input"
+    # USB devices
+    SUBSYSTEM=="usb", MODE="0660", GROUP="input"
+  '';
+  
+  # Add input-related packages
+  environment.systemPackages = with pkgs; [
+    xorg.xf86inputevdev
+    xorg.xf86inputlibinput
+    xorg.xkbcomp
+    xorg.xmodmap
+    evtest  # For debugging input issues
+  ];
+  
+  # Enable additional kernel modules that might be needed for input devices
+  boot.kernelModules = [ "hid" "usbhid" ];
+  
+  # Set up console keyboard just in case
+  console.useXkbConfig = true;
+  
+  # Ensure X11 is properly configured for the keyboard
+  services.xserver.deviceSection = ''
+    Option "AutoAddDevices" "true"
+  '';
+  
+  # Virtual console settings that are more ARM-friendly
+  console = {
+    packages = with pkgs; [ terminus_font ];
+    font = "ter-v18n";
+    keyMap = "us";
+  };
+  
+  # Additional X11 configuration files
+  environment.etc."X11/xorg.conf.d/00-keyboard.conf".text = ''
+    Section "InputClass"
+      Identifier "keyboard-all"
+      MatchIsKeyboard "on"
+      Driver "evdev"
+      Option "XkbLayout" "us"
+      Option "XkbOptions" "terminate:ctrl_alt_bksp"
+    EndSection
+  '';
+}
