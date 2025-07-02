71ef1b90647aab81da3591231a0ef52d2a7bc911
griefhound <borttappat@proton.me>
2025-07-02 14:32:05 +0200

    updates

--- scripts/bash/rofi2.sh
+++ scripts/bash/rofi2.sh
@@ -1,5 +1,43 @@
 #!/run/current-system/sw/bin/bash
-source ~/dotfiles/scripts/bash/detect-resolution.sh
 
-CONFIG="$HOME/.config/rofi/config${CONFIG_SUFFIX}.rasi"
-rofi -show "$1" -config "$CONFIG"
+eval $(xdotool getmouselocation --shell)
+
+CURRENT_DISPLAY=$(xrandr --listmonitors | awk -v x="$X" -v y="$Y" '
+$1 ~ /^[0-9]+:/ {
+    split($3, pos, /[x+]/)
+    split(pos[1], w, /\//)
+    split(pos[2], h, /\//)
+    if (x >= pos[3] && x < pos[3] + w[1] && y >= pos[4] && y < pos[4] + h[1]) {
+        print w[1] "x" h[1]
+        exit
+    }
+}')
+
+MONITOR_NAME=$(xrandr --listmonitors | awk -v x="$X" -v y="$Y" '
+$1 ~ /^[0-9]+:/ {
+    split($3, pos, /[x+]/)
+    split(pos[1], w, /\//)
+    split(pos[2], h, /\//)
+    if (x >= pos[3] && x < pos[3] + w[1] && y >= pos[4] && y < pos[4] + h[1]) {
+        print $4
+        exit
+    }
+}')
+
+echo "Display: $CURRENT_DISPLAY on monitor: $MONITOR_NAME"
+
+if echo "$CURRENT_DISPLAY" | grep -q "2880x1800"; then
+    CONFIG="~/dotfiles/rofi/config1800p.rasi"
+elif echo "$CURRENT_DISPLAY" | grep -q "2560x1440"; then
+    CONFIG="~/dotfiles/rofi/config4k.rasi"
+elif echo "$CURRENT_DISPLAY" | grep -q "1920x1080"; then
+    CONFIG="~/dotfiles/rofi/config1080p.rasi"
+elif echo "$CURRENT_DISPLAY" | grep -q "2288x1436"; then
+    CONFIG="~/dotfiles/rofi/config3k.rasi"
+elif echo "$CURRENT_DISPLAY" | grep -q "1920x1200"; then
+    CONFIG="~/dotfiles/rofi/config1200p.rasi"
+else
+    CONFIG="~/dotfiles/rofi/config.rasi"
+fi
+
+rofi "$@" -config "$CONFIG" -m "$MONITOR_NAME"
