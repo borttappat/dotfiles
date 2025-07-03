24f0984aa775a87e1eb21c2a66e75e053e698ccd
griefhound <borttappat@proton.me>
2025-07-03 09:34:46 +0200

    updates

--- modules/user-detection.nix
+++ modules/user-detection.nix
@@ -1,22 +1,23 @@
 { lib, ... }:
 
+let
+  sudoUser = builtins.getEnv "SUDO_USER";
+  currentUser = builtins.getEnv "USER";
+  detectedUser = if sudoUser != "" then sudoUser else currentUser;
+  finalUser = if detectedUser != "" then detectedUser else "traum";
+in
 {
   options = {
     currentUser = lib.mkOption {
       type = lib.types.str;
-      default = builtins.getEnv "SUDO_USER";
+      default = finalUser;
       description = "The current user running the configuration";
     };
     
     userHome = lib.mkOption {
       type = lib.types.str;
-      default = "/home/${builtins.getEnv "SUDO_USER"}";
+      default = "/home/${finalUser}";
       description = "Home directory of the current user";
     };
-  };
-  
-  config = lib.mkIf (builtins.getEnv "SUDO_USER" == "") {
-    currentUser = lib.mkDefault (builtins.getEnv "USER");
-    userHome = lib.mkDefault "/home/${builtins.getEnv "USER"}";
   };
 }
