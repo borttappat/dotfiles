dcf0f29a78aaf196891e922f84864e1535627434
griefhound <borttappat@proton.me>
2025-08-07 00:25:05 +0200

    setting up vm-specific hwconfig

--- modules/vm-hwconf.nix
+++ modules/vm-hwconf.nix
@@ -4,26 +4,19 @@
 imports = lib.optionals (builtins.pathExists /etc/nixos/hardware-configuration.nix) 
 [ /etc/nixos/hardware-configuration.nix ];
 
-# VM-friendly boot configuration
 boot.loader = {
-grub = {
-enable = true;
-device = "nodev";
-efiSupport = true;
-useOSProber = false;  # Usually not needed in VMs
-};
+systemd-boot.enable = true;
+grub.enable = false;
 efi = {
 canTouchEfiVariables = true;
 efiSysMountPoint = "/boot";
 };
 };
 
-# Basic VM hardware assumptions
 boot.initrd.availableKernelModules = [ 
 "ata_piix" "virtio_pci" "virtio_blk" "virtio_scsi" "sd_mod" "sr_mod" 
 ];
 boot.kernelModules = [ "kvm-intel" ];
 
-# Enable QEMU guest agent (correct syntax)
 services.qemuGuest.enable = true;
 }
