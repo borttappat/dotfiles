d6bf959c22ffa10fdc88f18e7cebfc9fe708fbc2
griefhound <borttappat@proton.me>
2025-08-08 23:18:57 +0200

    updates

--- modules/i3.nix
+++ modules/i3.nix
@@ -3,15 +3,10 @@
 with lib;
 
 {
-options.i3.enable = mkEnableOption "i3 window manager";
 
-config = mkIf config.i3.enable {
-services.xserver.enable = true;
 services.xserver.displayManager.startx.enable = true;
-services.xserver.windowManager.i3 = {
-enable = true;
-package = pkgs.i3-gaps;
-};
+services.xserver.windowManager.i3.enable = true;
+services.xserver.windowManager.i3.package = pkgs.i3-gaps;
 
 environment.systemPackages = with pkgs; [
 i3-gaps
@@ -27,14 +22,12 @@ dunst
 arandr
 lxappearance
 pavucontrol
-nitrogen
 xorg.xrandr
 xorg.xmodmap
 xclip
-scrot
-dmenu
 ];
 
+/*
 services.picom = {
 enable = true;
 fade = true;
@@ -49,12 +42,6 @@ inactiveOpacity = 0.85;
 backend = "glx";
 vSync = true;
 };
+*/
 
-fonts.packages = with pkgs; [
-cozette
-hack-font
-font-awesome
-powerline-fonts
-];
-};
 }
