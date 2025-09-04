8b291ff890e405a78eb8b78860c881bb1c676f7d
noneofya business <fuck@that.shit>
2025-09-04 11:32:52 +0200

    updates to vmhwconf

--- modules/vm-hwconf.nix
+++ modules/vm-hwconf.nix
@@ -1,11 +1,10 @@
 { config, lib, pkgs, ... }:
 {
   imports = [ /etc/nixos/hardware-configuration.nix ];
-
   boot.loader = lib.mkForce {
     grub = {
       enable = true;
-      device = "/dev/vda";  # or /dev/sda depending on your VM
+      device = "/dev/sda";
       efiSupport = false;
       useOSProber = false;
     };
