413a0bf9845de61da3c9d3c3edf9f0a2f3fb90f7
griefhound <borttappat@proton.me>
2025-10-07 10:28:18 +0200

    updates

--- scripts/bash/alacritty.sh
+++ scripts/bash/alacritty.sh
@@ -1,38 +1,57 @@
 #!/run/current-system/sw/bin/bash
 
-# Get cursor position
 eval $(xdotool getmouselocation --shell)
 
-# Get display info for cursor position  
 CURRENT_DISPLAY=$(xrandr --listmonitors | awk -v x="$X" -v y="$Y" '
 $1 ~ /^[0-9]+:/ {
-    split($3, pos, /[x+]/)
-    split(pos[1], w, /\//)
-    split(pos[2], h, /\//)
-    if (x >= pos[3] && x < pos[3] + w[1] && y >= pos[4] && y < pos[4] + h[1]) {
-        print w[1] "x" h[1]
-    }
-}')
+split($3, pos, /[x+]/)
+split(pos[1], w, /\//)
+split(pos[2], h, /\//)
+if (x >= pos[3] && x < pos[3] + w[1] && y >= pos[4] && y < pos[4] + h[1]) {
+gsub(/\/[0-9]+/, "", $3)
+print $3
+}
+}' | grep -oP '[0-9]{3,5}x[0-9]{3,5}' | head -n1)
 
 echo "Current display: $CURRENT_DISPLAY"
 
-# Choose config based on current display resolution
-if echo "$CURRENT_DISPLAY" | grep -q "2880x1800"; then
-    echo "Detected 1800p display - using 1800p config"
-    alacritty --config-file ~/dotfiles/alacritty/alacritty1800p.toml "$@"
-elif echo "$CURRENT_DISPLAY" | grep -q "2560x1440"; then
-    echo "Detected 1440p display - using 1440p config"
-    alacritty --config-file ~/dotfiles/alacritty/alacritty1440p.toml "$@"
-elif echo "$CURRENT_DISPLAY" | grep -q "1920x1080"; then
-    echo "Detected 1080p display - using 1080p config"
-    alacritty --config-file ~/dotfiles/alacritty/alacritty1080p.toml "$@"
-elif echo "$CURRENT_DISPLAY" | grep -q "2288x1436"; then
-    echo "Detected 3k display - using 3k config"
-    alacritty --config-file ~/dotfiles/alacritty/alacritty3k.toml "$@"
-elif echo "$CURRENT_DISPLAY" | grep -q "1920x1200"; then
-    echo "Detected 1200 display - using 1200p config"
-    alacritty --config-file ~/dotfiles/alacritty/alacritty1200p.toml "$@"
-else
-    echo "Using default config"
-    alacritty --config-file ~/dotfiles/alacritty/alacritty.toml "$@"
+CONFIG_FILE="$HOME/.config/display-config.json"
+HOSTNAME=$(hostnamectl hostname | cut -d'-' -f1)
+
+MACHINE_OVERRIDE=$(jq -r ".machine_overrides[\"$HOSTNAME\"] // null" "$CONFIG_FILE")
+
+if [ "$MACHINE_OVERRIDE" != "null" ]; then
+FORCED_RES=$(echo "$MACHINE_OVERRIDE" | jq -r '.force_resolution // "null"')
+if [ "$FORCED_RES" != "null" ]; then
+CURRENT_DISPLAY="$FORCED_RES"
 fi
+
+ALACRITTY_FONT_SIZE=$(echo "$MACHINE_OVERRIDE" | jq -r ".alacritty_font_size // null")
+ALACRITTY_SCALE_FACTOR=$(echo "$MACHINE_OVERRIDE" | jq -r ".alacritty_scale_factor // null")
+else
+ALACRITTY_FONT_SIZE="null"
+ALACRITTY_SCALE_FACTOR="null"
+fi
+
+RES_DEFAULTS=$(jq -r ".resolution_defaults[\"$CURRENT_DISPLAY\"] // null" "$CONFIG_FILE")
+
+if [ "$RES_DEFAULTS" = "null" ]; then
+echo "No defaults found for resolution: $CURRENT_DISPLAY, using 1920x1080 defaults"
+RES_DEFAULTS=$(jq -r '.resolution_defaults["1920x1080"]' "$CONFIG_FILE")
+fi
+
+[ "$ALACRITTY_FONT_SIZE" = "null" ] && ALACRITTY_FONT_SIZE=$(echo "$RES_DEFAULTS" | jq -r '.alacritty_font_size')
+
+if [ "$ALACRITTY_SCALE_FACTOR" != "null" ] && [ -n "$ALACRITTY_SCALE_FACTOR" ]; then
+ALACRITTY_SCALE_FACTOR_LINE="WINIT_X11_SCALE_FACTOR = \"$ALACRITTY_SCALE_FACTOR\""
+echo "Using font size: $ALACRITTY_FONT_SIZE with scale factor: $ALACRITTY_SCALE_FACTOR for display $CURRENT_DISPLAY"
+else
+ALACRITTY_SCALE_FACTOR_LINE=""
+echo "Using font size: $ALACRITTY_FONT_SIZE for display $CURRENT_DISPLAY"
+fi
+
+sed -e "s/\${ALACRITTY_FONT_SIZE}/$ALACRITTY_FONT_SIZE/g" \
+    -e "s/\${ALACRITTY_SCALE_FACTOR_LINE}/$ALACRITTY_SCALE_FACTOR_LINE/g" \
+    ~/.config/alacritty/alacritty.toml.template > ~/.config/alacritty/alacritty.toml
+
+exec alacritty "$@"
--- scripts/bash/autostart.sh
+++ scripts/bash/autostart.sh
@@ -3,50 +3,19 @@
 hostname=$(hostnamectl | grep "Icon name:" | cut -d ":" -f2 | xargs)
 
 if [[ ! $hostname =~ [vV][mM] ]]; then
-    killall -q picom
-    while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
-    picom -b
+killall -q picom
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+picom -b
 fi
 
-get_primary_resolution() {
-    xrandr | grep primary | grep -oP '\d+x\d+' | head -n1
-}
+source ~/.config/scripts/load-display-config.sh
+
+sed -e "s/\${POLYBAR_FONT_SIZE}/$POLYBAR_FONT_SIZE/g" \
+    ~/.config/polybar/config.ini.template > ~/.config/polybar/config.ini
 
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-PRIMARY_RESOLUTION=$(get_primary_resolution)
+polybar -q main &
 
-case $PRIMARY_RESOLUTION in
-    "2880x1800")
-        polybar -q hidpi &
-        ;;
-    "1920x1080")
-        polybar -q main &
-        ;;
-    "1920x1200")
-        polybar -q main &
-        ;;
-    "3840x2160")
-        polybar -q 4k &
-        ;;
-    "3600x2252")
-        polybar -q 3k &
-        ;;
-    "2288x1436")
-        polybar -q 3k &
-        ;;    
-    "2560x1440")
-        polybar -q 4k &
-        ;;
-    *)
-        polybar -q main &
-        ;;
-esac
-
-if xrandr | grep " connected" | grep -v "eDP" | grep -q "2560x1440"; then
-    echo "External 2560x1440 display detected, launching 4k polybar for external"
-    MONITOR=$(xrandr | grep " connected" | grep -v "eDP" | grep "2560x1440" | cut -d' ' -f1) polybar -q 4k &
-fi
-
-echo "Autostart completed... Primary: $PRIMARY_RESOLUTION, External 2560x1440: $(xrandr | grep " connected" | grep -v "eDP" | grep -q "2560x1440" && echo 'Yes' || echo 'No')"
+echo "Autostart completed... Resolution: $DISPLAY_RESOLUTION, Polybar font: $POLYBAR_FONT_SIZE"
